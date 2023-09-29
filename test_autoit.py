0ca1e807cef57d008d7757f8c00932388b1977c8
Sicambr <sicambr84@gmail.com>
2023-09-29 15:40:24 +0700

    chaneged auction house prices

--- test_autoit.py
+++ test_autoit.py
@@ -1,42 +1,34 @@
-class Firstclass:
-    def setdata(self, value):
-        self.data = value
-
-    def display(self):
-        print(self.data)
+import random
+import json
+from push_info import load_data, load_prices
 
 
-class Secondclass(Firstclass):
-    def display(self):
-        print('Current value = "%s"' % self.data)
+all_item_settings = json.load(open('big_data_tables.json'))
+item_settings = all_item_settings['Armor_(White_Blue_Yellow_Grade)']
+start_level = 8
+
+def get_dif(start_level):
+    difference = (item_settings[str(start_level)]['1'] - 
+                  item_settings[str(start_level)]['0']) * 100000
+    return int(difference)
+
+def one_upgrade(difference, start_level, fail_up = 1):
+    fails = 0
+    while True:
+        number = random.randint(1, 10000000)
+        if item_settings[str(start_level)].get(str(fails)):
+            chance = item_settings[str(start_level)][str(fails)] * 100000
+        else:
+            chance += difference*fail_up
+            if chance > 9000000:
+                chance = 9000000
+        if number <= chance:
+            print('ok')
+            break
+        else:
+            fails += fail_up
+            print(f'fails {fails}, chance {chance / 100000}')
 
 
-class rec:
-    pass
-
-
-x = Firstclass()
-y = Firstclass()
-
-x.setdata('King Artur')
-y.setdata(34)
-
-x.display()
-y.display()
-
-x.data = ('nemo')
-x.display()
-
-x.anonthername = 'lel'
-print(x.anonthername)
-
-z = Secondclass()
-z.setdata(42)
-z.display()
-
-x = rec()
-rec.name = 'Bob'
-y = rec()
-print(x.name)
-print(y.name)
-print(rec.__dict__)
+difference = get_dif(start_level)
+one_upgrade(difference, start_level)
